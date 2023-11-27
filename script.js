a82a6c0356c16dfe45a49eb95a19d15d710fc558
PreshoBz <90356704+PreshoBz@users.noreply.github.com>
2023-11-27 13:54:57 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // First API call to get public IP address
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            const ipElement = document.getElementById('ip');
+            ipElement.innerText = `Your public IP address is: ${data.ip}`;
+
+            // Second API call to get location based on the public IP address
+            return fetch(`https://ipapi.co/${data.ip}/json/`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            const locationElement = document.getElementById('location');
+            locationElement.innerText = `Your location is: ${data.city}, ${data.region}, ${data.country_name}`;
+
+            // Third API call to get weather based on the public IP address
+            return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=YOUR_OPENWEATHERMAP_API_KEY`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            const weather1Element = document.getElementById('weather1');
+            weather1Element.innerText = `The weather at your location is: ${data.weather[0].description}`;
+        })
+        .catch(error => console.error('Error:', error));
+});
